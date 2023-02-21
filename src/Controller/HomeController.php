fe0d8123cd4af16af80f0a8a927d8a4242b8ac71
DavyDesign <davydesign@hotmail.fr>
2023-02-21 16:17:36 +0100

    21/02/2023

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,8 +16,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(FreelanceRepository $freelanceRepository)
     {
-        return $this->render('home/index.html.twig',[
-        'freelances' => $freelanceRepository->findAll(),
+        return $this->render('home/index.html.twig', [
+            'freelances' => $freelanceRepository->findAll(),
         ]);
 
     }
